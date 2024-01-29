e1d117129301a76cd096fe0580f95228232cb3da
Pranshu <pranshu2.71828@gmail.com>
2024-01-29 21:13:36 +0530

    completed 3d effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import { useState } from 'react'
-import './App.css'
+import { React, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Tetrahedron } from '@react-three/drei'
+import { pointsAsteriod, pointsCircle, pointsOuter } from './utils/pointCalc'
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className='relative'>
+      <Canvas className='bg-[#101010]' style={{ height: '100vh' }} camera={{ position: [10, -7.5, -5], }}>
+        <OrbitControls maxDistance={17} minDistance={10} />
+        <directionalLight />
+        <pointLight position={[-30, 0, -30]} power={25.0} />
+        <TetrahedronElems />
+      </Canvas>
+      <h1 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-slate-200 font-medium text-2xl md:text-5xl pointer-events-none">
+        Placeholder
+      </h1>
+    </div>
+  )
+}
+
+const TetrahedronElems = () => {
+  const ref = useRef();
+
+  useFrame(({ clock }) => {
+    ref.current.rotation.z = clock.getElapsedTime() * 0.05;
+  });
+
+  return (
+    <group ref={ref}>
+      {pointsAsteriod.map(point => <Tetra key={point.idx} position={point.position} color={point.color} />)}
+      {pointsCircle.map(point => <Tetra key={point.idx} position={point.position} color={point.color} />)}
+      {pointsOuter.map(point => <Tetra key={point.idx} position={point.position} color={point.color} />)}
+    </group>
+  )
+}
+
+const Tetra = ({ position, color }) => {
+  return (
+    <Tetrahedron
+      position={position}
+      args={[0.1, 1, 1]}
+    >
+      <meshStandardMaterial
+        color={color}
+        emissive={color}
+        emissiveIntensity={0.5}
+        roughness={0.5}
+      />
+    </Tetrahedron>
   )
 }
 
---
+++ src/utils/pointCalc.jsx
@@ -0,0 +1,134 @@
+const Min_Radius = 7.5;
+const Max_Radius = 15;
+const Depth = 2;
+const Left_Color = "F79A00";
+const Right_Color = "FD12D1";
+const Num_Tetra = 2500
+
+const randomBetweenInterval = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
+const getGradientStop = (ratio) => {
+  ratio = ratio > 1 ? 1 : ratio < 0 ? 0 : ratio
+
+  const c0 = Left_Color.match(/.{1,2}/g).map(
+    (oct) => parseInt(oct, 16) * (1 - ratio)
+  )
+  const c1 = Right_Color.match(/.{1,2}/g).map(
+    (oct) => parseInt(oct, 16) * (ratio)
+  )
+
+  const ci = [0, 1, 2].map((i) => Math.min(Math.round(c0[i] + c1[i]), 255))
+  const color = ci
+    .reduce((a, v) => (a << 8) + v, 0)
+    .toString(16)
+    .padStart(6, '0')
+
+  return `#${color}`
+}
+
+const calcColor = (x) => {
+  const maxDiff = Max_Radius * 2
+  const dist = x + maxDiff
+  const ratio = dist / maxDiff
+  const stop = getGradientStop(ratio)
+
+  return stop
+}
+
+export const pointsAsteriod = Array.from(
+  { length: Num_Tetra },
+  (_, k) => k + 1
+).map((num) => {
+  const randomRadius = randomBetweenInterval(Min_Radius, Max_Radius)
+  const randomAngle = Math.random() * Math.PI * 5
+
+  // const x = randomRadius * Math.cos(randomAngle)
+  // const y = randomRadius * (Math.sin(randomAngle) + 0.2 * Math.cos(2.8 * randomAngle))
+
+  // const x = randomRadius * (2 * Math.cos(randomAngle) + Math.cos(2 * randomAngle))
+  // const y = randomRadius * (2 * Math.sin(randomAngle) + Math.sin(2 * randomAngle))
+
+  // const x = randomRadius * Math.sin(randomAngle) * (Math.exp(Math.cos(randomAngle)) - 2 * Math.cos(4 * randomAngle) - Math.pow(Math.sin(randomAngle / 12), 5))
+  // const y = randomRadius * Math.cos(randomAngle) * (Math.exp(Math.cos(randomAngle)) - 2 * Math.cos(4 * randomAngle) - Math.pow(Math.sin(randomAngle / 12), 5))
+
+  const x = randomRadius * (Math.pow(Math.cos(randomAngle), 3))
+  const y = randomRadius * (Math.pow(Math.sin(randomAngle), 3))
+
+  const z = randomBetweenInterval(-Depth, Depth)
+
+  const color = calcColor(x)
+
+  return {
+    idx: num,
+    position: [x, y, z],
+    color,
+  };
+});
+
+export const pointsCircle = Array.from(
+  { length: Num_Tetra },
+  (_, k) => k + 1
+).map((num) => {
+  const randomRadius = randomBetweenInterval(Min_Radius, Max_Radius)
+  const randomAngle = Math.random() * Math.PI * 5
+
+  // const x = randomRadius * Math.cos(randomAngle)
+  // const y = randomRadius * (Math.sin(randomAngle) + 0.2 * Math.cos(2.8 * randomAngle))
+
+  // const x = randomRadius * (2 * Math.cos(randomAngle) + Math.cos(2 * randomAngle))
+  // const y = randomRadius * (2 * Math.sin(randomAngle) + Math.sin(2 * randomAngle))
+
+  // const x = randomRadius * Math.sin(randomAngle) * (Math.exp(Math.cos(randomAngle)) - 2 * Math.cos(4 * randomAngle) - Math.pow(Math.sin(randomAngle / 12), 5))
+  // const y = randomRadius * Math.cos(randomAngle) * (Math.exp(Math.cos(randomAngle)) - 2 * Math.cos(4 * randomAngle) - Math.pow(Math.sin(randomAngle / 12), 5))
+
+  // const y = randomRadius * (Math.pow(Math.sin(randomAngle), 3))
+  // const x = randomRadius * (Math.pow(Math.cos(randomAngle), 3))
+
+  const x = randomRadius * Math.cos(randomAngle)
+  const y = randomRadius * Math.sin(randomAngle)
+
+  const z = randomBetweenInterval(-Depth, Depth)
+
+  const color = calcColor(x)
+
+  return {
+    idx: num,
+    position: [x, y, z],
+    color,
+  };
+});
+
+export const pointsOuter = Array.from(
+  { length: Num_Tetra },
+  (_, k) => k + 1
+).map((num) => {
+  const randomRadius = randomBetweenInterval(Min_Radius / 2, Max_Radius * 2)
+  const randomAngle = Math.random() * Math.PI * 5
+
+  // const x = randomRadius * Math.cos(randomAngle)
+  // const y = randomRadius * (Math.sin(randomAngle) + 0.2 * Math.cos(2.8 * randomAngle))
+
+  // const x = randomRadius * (2 * Math.cos(randomAngle) + Math.cos(2 * randomAngle))
+  // const y = randomRadius * (2 * Math.sin(randomAngle) + Math.sin(2 * randomAngle))
+
+  // const x = randomRadius * Math.sin(randomAngle) * (Math.exp(Math.cos(randomAngle)) - 2 * Math.cos(4 * randomAngle) - Math.pow(Math.sin(randomAngle / 12), 5))
+  // const y = randomRadius * Math.cos(randomAngle) * (Math.exp(Math.cos(randomAngle)) - 2 * Math.cos(4 * randomAngle) - Math.pow(Math.sin(randomAngle / 12), 5))
+
+  // const y = randomRadius * (Math.pow(Math.sin(randomAngle), 3))
+  // const x = randomRadius * (Math.pow(Math.cos(randomAngle), 3))
+
+  const x = randomRadius * Math.cos(randomAngle)
+  const y = randomRadius * Math.sin(randomAngle)
+
+  const z = randomBetweenInterval(-Depth * 10, Depth * 10)
+
+  const color = calcColor(x)
+
+  return {
+    idx: num,
+    position: [x, y, z],
+    color,
+  };
+});
